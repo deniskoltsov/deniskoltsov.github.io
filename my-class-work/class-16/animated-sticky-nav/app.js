47933f7f84b4d89639ad209f8f3ee32430c1e0e5
deniskoltsov <denis_koltsov@yahoo.com>
2016-01-05 18:33:27 -0500

    sticky nav

--- my-class-work/class-16/animated-sticky-nav/app.js
+++ my-class-work/class-16/animated-sticky-nav/app.js
@@ -1,2 +1,14 @@
 $(document).ready(function () {
+
+	$(window).scroll(function () {
+
+		// console.log($(this).scrollTop())
+
+		if ($(this).scrollTop() > 100) {
+			$('header').addClass('sticky')
+		} else {
+			$('header').removeClass('sticky')
+		}
+	})
+
 })
